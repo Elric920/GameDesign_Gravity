6770cb0b665f920ca04a81b117bf70b7235eb25f
kkcooo <68552409+kkcooo@users.noreply.github.com>
2020-12-08 21:52:34 +0800

    半稳定石更新
    Du 2020 1208 21:48
    更新半稳定石
    运动规律：匀速上下运动
    可设置的参数：向上的速度semiUpVelocity、向下的速度semiDownVelocity、
                             运动范围movementDistance

--- Master of fostering/Assets/Scripts/semiStone.cs
+++ Master of fostering/Assets/Scripts/semiStone.cs
@@ -2,80 +2,144 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/**********************************
+ * 半稳定石运动规律
+ * 1 向上匀加速到semiV
+ * 2 向上匀速运动指定距离
+ * 3 向上匀减速到0
+ * 4 向下匀加速到semiV
+ * 5 向下匀速运动指定距离
+ * 6 向下匀减速到0
+ * 7 循环
+ * 
+ * 
+ * *******************************/
+
+
+
 public class semiStone : MonoBehaviour
 {
     private Transform semiStoneTrans; //半稳定石的位置 
     private Rigidbody2D rb2D;//半稳定石的rb2D组件
 
-    public float initialVelocity = 0;//半稳定石运动的初速度
-    public float acceleratedSpeed = 0;//半稳定石运动的加速度
 
-    private Vector2 semiStoneInitialPosition;//记录半稳定石初始的位置
-    private Vector2 semiStonepresentPosition;//记录半稳定石当前的位置
-    private int positionState=0;//记录半稳定石的位置状态：1为下，2为上
+    //公开的变量
+    public float semiUpVelocity = 0f;//半稳定石向上的运动速度
+    public float semiDownVelocity = 0f;//半稳定石向下的运动速度
+    public float acceleratedSpeed = 0f;//半稳定石运动的加速度
+    public float movementDistance = 0f;//半稳定石的运动范围
 
+    //半稳定石位置判断
+    private Vector2 semiP0;//记录半稳定石初始的位置
+    private Vector2 semip;//记录半稳定石当前的位置
+    Vector3 position = new Vector3(0, 0, 0);
+    private int semiStoneMoveState = 0;//记录半稳定石的运动状态：1为向上匀加速，2为向上匀速，3为向上匀减速
+                                       // 4为向下匀加速，5为向下匀速，6为向下匀减速
+
+    //控制半稳定石速度
     private Vector2 presentVelocity = new Vector2(0f, 0f);//记录半稳定石当前的速度
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         semiStoneTrans = this.GetComponent<Transform>();
         rb2D = this.GetComponent<Rigidbody2D>();
 
-        semiStoneInitialPosition = semiStoneTrans.position;//游戏开始，记录半稳定石位置
+        semiP0 = semiStoneTrans.position;//游戏开始，记录半稳定石位置
 
-        presentVelocity.y = -initialVelocity;//游戏开始，给半稳定石一个向下的初速度
-        rb2D.velocity = presentVelocity;
     }
 
     // Update is called once per frame
     void Update()
     {
-        SemiStonePosion();//判断当前半稳定石位置
+        SemiStoneMoveState();//半稳定石运动状态判断
         SemiStoneMove();//半稳定石运动
     }
 
-    void SemiStonePosion()//半稳定石位置判断
+    void SemiStoneMoveState()//半稳定石运动状态判断
     {
-        semiStonepresentPosition = semiStoneTrans.position;
-        if (semiStoneInitialPosition.y > semiStonepresentPosition.y)//半稳定石当前位置低于半稳定石初始位置
-            positionState = 1;
-        if (semiStoneInitialPosition.y== semiStonepresentPosition.y)
-            positionState = 0;
-        if (semiStoneInitialPosition.y < semiStonepresentPosition.y)
-            positionState = 2;
+        semip = semiStoneTrans.position;
+        if(semiP0.y==semip.y) semiStoneMoveState = 2;
+        if ((semip.y - semiP0.y) >= 0 && (semip.y - semiP0.y) <= movementDistance)
+            if (rb2D.velocity.y > 0) semiStoneMoveState = 2;
+        if ((semip.y - semiP0.y) > movementDistance)
+        {
+            position = this.transform.position;
+            position.y = semiP0.y + movementDistance;
+            this.transform.position = position;
+            semiStoneMoveState = 5;
+        }
+        if ((semip.y - semiP0.y) == movementDistance) semiStoneMoveState = 5;
+        if ((semip.y - semiP0.y) >= 0 && (semip.y - semiP0.y) <= movementDistance)
+            if (rb2D.velocity.y < 0) semiStoneMoveState = 5;
+        if (semip.y < semiP0.y)
+        {
+            position = this.transform.position;
+            position.y = semiP0.y;
+            this.transform.position = position;
+            semiStoneMoveState = 2;
+        }
     }
 
     private void SemiStoneMove() //半稳定石的运动
     {
-        if (positionState == 1)//半稳定石加速度向上的阶段
+        presentVelocity = rb2D.velocity;
+        //if (semiStoneMoveState == 1)//半稳定石加速度向上的阶段
+        //{
+        //    presentVelocity.y += acceleratedSpeed * Time.deltaTime;
+        //    if (presentVelocity.y >= semiVelocity) presentVelocity.y = semiVelocity;//速度加过头了
+        //    rb2D.velocity = presentVelocity;
+        //    print("加速向上");
+        //}
+        if (semiStoneMoveState == 2)//半稳定石匀速向上阶段
         {
-            presentVelocity.y += acceleratedSpeed * Time.deltaTime;
+            presentVelocity.y = semiUpVelocity;
             rb2D.velocity = presentVelocity;
+            print("匀速向上");
+        }
 
-        }
-        if (positionState ==2)//半稳定石加速度向下的阶段
+        //if (semiStoneMoveState == 3)//半稳定石匀减速向上的阶段
+        //{
+        //    presentVelocity.y -= acceleratedSpeed * Time.deltaTime;
+        //    if (presentVelocity.y <= 0) presentVelocity.y = 0;//速度减过头了
+        //    rb2D.velocity = presentVelocity;
+        //    print("减速向上");
+        //}
+        //if (semiStoneMoveState == 4)//半稳定石匀加速向下的阶段
+        //{
+        //    presentVelocity.y -= acceleratedSpeed * Time.deltaTime;
+        //    if (presentVelocity.y <= -semiVelocity)//速度加过头了
+        //        rb2D.velocity = presentVelocity;
+        //    print("加速向下");
+        //}
+        if (semiStoneMoveState == 5)//半稳定石匀速向下的阶段
         {
-            presentVelocity.y -= acceleratedSpeed * Time.deltaTime;
-            rb2D.velocity = presentVelocity;
+            presentVelocity.y = -semiDownVelocity;
+            rb2D.velocity = presentVelocity; 
+            print("匀速向下");
         }
+        //if (semiStoneMoveState == 6)//半稳定石匀减速向下的阶段
+        //{
+        //    presentVelocity.y += acceleratedSpeed * Time.deltaTime;
+        //    if (presentVelocity.y >= 0) presentVelocity.y = 0;//速度减过头了
+        //    rb2D.velocity = presentVelocity;
+        //    print("减速向下");
+        //}
     }
+
+
 
     //private void OnGUI()
     //{
     //    if (GUILayout.Button("开始"))
     //    {
-            
-            
 
     //    }
     //    if (GUILayout.Button("停止"))
     //    {
-           
+
     //    }
     //}
-
-
-
-    //左右
-
 }
